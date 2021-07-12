5b6776605988a14c3ef724e1fe23f16a532a9b17
kimberly ong <e0424807@u.nus.edu>
2021-07-12 17:36:35 +0800

    feat: Set up TaskList component

---
+++ components/TaskList.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
+import {useSelector} from 'react-redux';
+import {TaskState} from '../redux/taskReducer';
+import {Task} from './Task';
+
+export const TaskList = () => {
+  const tasks = useSelector<TaskState, TaskState['tasks']>(
+    state => state.tasks,
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList data={tasks} renderItem={({item}) => <Task {...item} />} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
