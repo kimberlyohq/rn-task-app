ee14f1aa14eb9c9de7a0e8a9f958255acdcdea8e
kimberly ong <e0424807@u.nus.edu>
2021-07-12 17:37:59 +0800

    chore: add files

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {HomeScreen} from './screens/HomeScreen';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              title: 'tasks 🧸',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
